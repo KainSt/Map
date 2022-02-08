36653b6766d38e86c5702ff119abb7d3cda514ab
KainSt <ksfgnw@gmail.com>
2022-02-09 00:35:16 +0300

    Jsoup parser

--- src/com/company/MapJsonParser.java
+++ src/com/company/MapJsonParser.java
@@ -7,23 +7,31 @@ import org.jsoup.select.Elements;
 import java.io.IOException;
 
 import java.net.URL;
+import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 public class MapJsonParser {
     public static void main(String[] args){
-        List<String> nameList = new LinkedList<>();
+        List<String> stationList = new LinkedList<>();
+        Map<String, String> linesNameList = new LinkedHashMap<>();
+
         try{
-            Document doc = Jsoup.parse(new URL("https://www.moscowmap.ru/metro.html#lines"),3000);
+            Document doc = Jsoup.connect("https://www.moscowmap.ru/metro.html#lines").maxBodySize(0).get();
             Elements metroTable = doc.select("#metrodata");
-            Elements lines = metroTable.select("div[class=js-metro-stations t-metrostation-list-table]");
-            Elements station = lines.select("span[class=name]");
-            station.forEach(element -> nameList.add(element.text()));
+            Elements lines = metroTable.select("span[data-line]");
+            Elements station = metroTable.select("a[data-metrost]");
+
+
+            lines.forEach(element -> linesNameList.put(element.attr("data-line") , element.text()));
+            station.forEach(element -> stationList.add(element.text()));
             System.out.println();
         }
         catch (IOException exception){
             exception.getStackTrace();
         }
         System.out.println();
+
     }
 }
